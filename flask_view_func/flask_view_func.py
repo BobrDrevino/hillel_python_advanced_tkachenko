ec1179dc0aa42843836dd500322c61d298f94815
Ilya Tkachenko <tv.ilya1997@gmail.com>
2022-09-22 17:03:23 +0300

    View Func 2.0

--- flask_view_func/flask_view_func.py
+++ flask_view_func/flask_view_func.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from datetime import datetime
 from get_currency import get_currency_exchange_rate_nbu
 from get_currency import get_currency_exchange_rate_pb
+from db_funcrions import get_filtered_customers, count_customers_firstname, sum_of_orders
 
 app = Flask(__name__)
 
@@ -26,6 +27,24 @@ def get_rates():
         result = get_currency_exchange_rate_pb(currency_a, currency_b, date)
 
     return result
+
+
+@app.route("/filtered_customers", methods=['GET'])
+def filtered_customers():
+    city = request.args.get('city', default=None)
+    state = request.args.get('state', default=None)
+
+    return get_filtered_customers(city=city, state=state)
+
+
+@app.route("/count_first_name", methods=['GET'])
+def count_first_name():
+    return count_customers_firstname()
+
+
+@app.route("/total_amount", methods=['GET'])
+def total_amount():
+    return sum_of_orders()
 
 
 if __name__ == '__main__':
